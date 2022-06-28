f5b1ee0966d46f516b13968bd2996cea97acefcb
Ashwin Sundar <ashiundar@gmail.com>
2022-06-27 21:15:25 -0400

    Create split_strings.rs

---
+++ split_strings.rs
@@ -0,0 +1,30 @@
+// Codewars - Split Strings -> https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/rust
+fn split_strings(s: &str) -> Vec<String> {
+    let mut split: Vec<String> = Vec::new();
+    let mut chars = s.chars();
+
+    for _i in 0..(s.len() / 2) {
+        let mut t: String = chars.next().unwrap().to_string();
+        t.push(chars.next().unwrap());
+        split.push(t);
+    }
+
+    // bitwise implementation of modulo 2
+    if s.len() & 1 == 1 {
+        let mut t: String = chars.next().unwrap().to_string();
+        t.push('_');
+        split.push(t);
+        return split;
+    }
+    split
+}
+  
+  #[test]
+fn test_split_strings() {
+    assert_eq!(split_strings("a"), vec!["a_"]);
+    assert_eq!(split_strings("ab"), vec!["ab"]);
+    assert_eq!(split_strings("abc"), vec!["ab", "c_"]);
+    assert_eq!(split_strings("abcd"), vec!["ab", "cd"]);
+    assert_eq!(split_strings("abcde"), vec!["ab", "cd", "e_"]);
+    assert_eq!(split_strings("abcdef"), vec!["ab", "cd", "ef"]);
+}
